15c0b0df01cd3c87287adfbe96d19f722ac530a7
galeyro <galo.guevara.torres@udla.edu.ec>
2025-04-08 08:19:55 -0500

    Agregar archivos de proyecto.

---
+++ GuevaraG_LigaPro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GuevaraG_LigaPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

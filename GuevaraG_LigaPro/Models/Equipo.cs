5903c4b436117a57b6c29724c4d767100b652de2
galeyro <galo.guevara.torres@udla.edu.ec>
2025-04-08 08:23:28 -0500

    Creacion modelo Equipo

---
+++ GuevaraG_LigaPro/Models/Equipo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuevaraG_LigaPro.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int PartidosJugados { get; set; }
+        public int PartidosGanados { get; set; }
+        public int PartidosEmpatados { get; set; }
+        public int PartidosPerdidos { get; set; }
+        public int TotalPuntos { get; set; }
+    }
+}

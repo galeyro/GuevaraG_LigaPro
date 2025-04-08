c6e1429054131380405640999ca9170d58a7f9d8
galeyro <galo.guevara.torres@udla.edu.ec>
2025-04-08 08:54:34 -0500

    Implementacion Listas

---
+++ GuevaraG_LigaPro/Controllers/EquipoController.cs
@@ -0,0 +1,39 @@
+﻿using GuevaraG_LigaPro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuevaraG_LigaPro.Controllers
+{
+    public class EquipoController : Controller
+    {
+        public IActionResult List()
+        {
+            List<Equipo> equipos = new List<Equipo>();
+            Equipo ldu = new Equipo
+            {
+                Id = 1,
+                Nombre = "LDU",
+                PartidosJugados = 10,
+                PartidosGanados = 10,
+                PartidosEmpatados = 0,
+                PartidosPerdidos = 0,
+                TotalPuntos = 30
+            };
+
+            Equipo bcs = new Equipo
+            {
+                Id = 2,
+                Nombre = "BCS",
+                PartidosJugados = 10,
+                PartidosGanados = 1,
+                PartidosEmpatados = 0,
+                PartidosPerdidos = 9,
+                TotalPuntos = 3
+            };
+
+            equipos.Add(ldu);
+            equipos.Add(bcs);
+
+            return View(equipos);
+        }
+    }
+}
--- GuevaraG_LigaPro/Models/Equipo.cs
+++ GuevaraG_LigaPro/Models/Equipo.cs
@@ -6,10 +6,16 @@ namespace GuevaraG_LigaPro.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Nombre { get; set; }
+        [Range(0,20)]
         public int PartidosJugados { get; set; }
+        [Range(0, 20)]
         public int PartidosGanados { get; set; }
+        [Range(0, 20)]
         public int PartidosEmpatados { get; set; }
+        [Range(0, 20)]
         public int PartidosPerdidos { get; set; }
         public int TotalPuntos { get; set; }
     }

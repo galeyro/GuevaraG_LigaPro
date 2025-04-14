d88a1f56b9ec486eed02ce6b422433cd08bfc019
galeyro <galo.guevara.torres@udla.edu.ec>
2025-04-14 17:31:26 -0500

    Adicion de funcionalidad Editar Equipo v1

--- GuevaraG_LigaPro/Controllers/EquipoController.cs
+++ GuevaraG_LigaPro/Controllers/EquipoController.cs
@@ -30,5 +30,46 @@ namespace GuevaraG_LigaPro.Controllers
 
             return View(equipo);
         }
+
+        //Accion editar equipo
+        public IActionResult EditarEquipo(int id)
+        {
+            EquipoRepository repositorio = new EquipoRepository();
+            //Buscar el equipo por Id usando LINQ
+            var equipo = repositorio.DevuelveListadoEquipos().FirstOrDefault(e => e.Id == id);
+            //Validar si el equipo existe
+            if (equipo == null)
+            {
+                return NotFound(); //Error 404
+            }
+            return View(equipo);
+        }
+
+        [HttpPost] //Acción del controlador para ejecutar en solicitud HTTP tipo POST
+        public IActionResult EditarEquipo(Equipo equipo)
+        {
+            if (ModelState.IsValid) //Valida si cumple con las reglas del modelo
+            {
+                // Aquí puedes actualizar los datos en el repositorio o base de datos
+                EquipoRepository repositorio = new EquipoRepository();
+                var equipos = repositorio.DevuelveListadoEquipos().ToList();
+                var equipoExistente = equipos.FirstOrDefault(e => e.Id == equipo.Id);
+
+                if (equipoExistente != null)
+                {
+                    equipoExistente.PartidosJugados = equipo.PartidosJugados;
+                    equipoExistente.PartidosGanados = equipo.PartidosGanados;
+                    equipoExistente.PartidosEmpatados = equipo.PartidosEmpatados;
+                    equipoExistente.PartidosPerdidos = equipo.PartidosPerdidos;
+                    equipoExistente.CalcularPuntos(); // Recalcular los puntos
+                }
+                //Al guardar cambios, redirige al usuario de vuelta a la lista de equipos
+                return RedirectToAction("List");
+            }
+            
+            //Si los datos no son válidos, volver a mostrar la vista edición
+            return View(equipo);
+        }
+
     }
 }

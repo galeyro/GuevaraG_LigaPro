ddab1f00b1f516e4e269a2ae84424c3d064686f6
galeyro <galo.guevara.torres@udla.edu.ec>
2025-04-13 21:45:21 -0500

    Adición acción para mostrar los detalles de un equipo en EquipoController.cs

--- GuevaraG_LigaPro/Controllers/EquipoController.cs
+++ GuevaraG_LigaPro/Controllers/EquipoController.cs
@@ -6,12 +6,29 @@ namespace GuevaraG_LigaPro.Controllers
 {
     public class EquipoController : Controller
     {
+        //Acción para mostrar la lista de equipos
         public IActionResult List()
         {
             EquipoRepository repositorio = new EquipoRepository();
             var equipos = repositorio.DevuelveListadoEquipos();
 
             return View(equipos);
+        }
+
+        // Acción para mostrar los detalles de un equipo
+        public IActionResult Details(int id)
+        {
+            EquipoRepository repositorio = new EquipoRepository();
+
+            //Buscar el equipo por Id usando LINQ
+            var equipo = repositorio.DevuelveListadoEquipos().FirstOrDefault(e => e.Id == id);
+            //Validar si el equipo existe
+            if (equipo == null)
+            {
+                return NotFound(); //Error 404
+            }
+
+            return View(equipo);
         }
     }
 }
